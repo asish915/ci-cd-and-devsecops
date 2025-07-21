2951cf403e0e43c7b594a6c44f4be27841b46629
AsishSarangi <asishsarangi2005@gmail.com>
2025-07-21 12:06:13 +0530

    automate

--- variable.tf
+++ variable.tf
@@ -11,5 +11,5 @@ variable "bucket_name" {
 variable "kms_key_arn" {
   description = "KMS Key ARN used for decryption"
   type        = string
-  default     = "arn:aws:kms:ap-south-1:068760013706:key/0736d14e-d38f-41de-a71e-18313d9ee4b7"
+  default     = "arn:aws:kms:ap-south-1:068760013706:key/22fc48e3-ded2-48c6-baf4-1bf91df4f85b" # change it
 }
